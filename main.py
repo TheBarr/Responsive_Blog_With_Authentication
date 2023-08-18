754891c75c1f2b21f8fe438ef2993559e3c6e467
bartek <bartex600700@gmail.com>
2023-08-18 22:17:30 +0200

    deploy

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ gravatar = Gravatar(
 )
 
 # CONNECT TO DB
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URI")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URI", "sqlite:///blog.db")
 db = SQLAlchemy()
 db.init_app(app)
 
